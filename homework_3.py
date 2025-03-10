65cfb1a936213a0c9d60e16c9161daeabe2c6130
Tookebaeva <elfelie.dev@gmail.com>
2025-03-10 12:36:27 +0600

    homework 3

--- homework_3.py
+++ homework_3.py
@@ -5,13 +5,10 @@ class Computer:
 
     def get_cpu(self):
         return self.__cpu
-
     def set_cpu(self, cpu: float):
         self.__cpu = cpu
-
     def get_memory(self):
         return self.__memory
-
     def set_memory(self, memory: int):
         self.__memory = memory
 
@@ -24,23 +21,17 @@ class Computer:
         }
 
     def __str__(self):
-        return f"Computer(cpu={self.__cpu}, memory={self.__memory})"
-
+        return f"cpu={self.__cpu}, memory={self.__memory}"
     def __eq__(self, other):
         return self.__memory == other.__memory
-
     def __ne__(self, other):
         return self.__memory != other.__memory
-
     def __lt__(self, other):
         return self.__memory < other.__memory
-
     def __le__(self, other):
         return self.__memory <= other.__memory
-
     def __gt__(self, other):
         return self.__memory > other.__memory
-
     def __ge__(self, other):
         return self.__memory >= other.__memory
 
@@ -48,21 +39,16 @@ computer = Computer(3.5, 16)
 print(computer)
 print(computer.make_computations())
 
-
 class Phone:
     def __init__(self, sim_cards_list):
         self.__sim_cards_list = sim_cards_list
-
     def get_sim_cards_list(self):
         return self.__sim_cards_list
-
     def set_sim_cards_list(self, sim_cards_list):
         self.__sim_cards_list = sim_cards_list
-
     def call(self, sim_card_number, call_to_number):
         if 1 <= sim_card_number <= len(self.__sim_cards_list):
             print(f"идет звонок на номер {call_to_number} с сим-карты - {self.__sim_cards_list[sim_card_number - 1]}")
-
     def __str__(self):
         return f"Phone(sim_cards_list={self.__sim_cards_list})"
 
@@ -74,16 +60,10 @@ class SmartPhone(Computer, Phone):
     def __init__(self, cpu, memory, sim_cards_list):
         Computer.__init__(self, cpu, memory)
         Phone.__init__(self, sim_cards_list)
-
-    def use_gps(self, location):
-        print(f"прокладывается маршрут до {location}...")
-
     def __str__(self):
         return f"SmartPhone(cpu={self.get_cpu()}, memory={self.get_memory()}, sim_cards_list={self.get_sim_cards_list()})"
 
-
 smartphone1 = SmartPhone(2.8, 8, ["O!", "Tel2"])
 print(smartphone1)
-smartphone1.use_gps("Бишкек")
 
 print(computer > smartphone1)
