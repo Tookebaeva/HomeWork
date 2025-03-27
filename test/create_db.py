e13c64bac0616a02e9f5e72b00ad87071f1638cd
Tookebaeva <elfelie.dev@gmail.com>
2025-03-27 19:02:36 +0600

    pract

--- test/create_db.py
+++ test/create_db.py
@@ -2,7 +2,7 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
 
-with open('database.sql', 'r', encoding='utf-8') as f:
+with open('data.sql', 'r', encoding='utf-8') as f:
     sql_script = f.read()
 cursor.executescript(sql_script)
 
